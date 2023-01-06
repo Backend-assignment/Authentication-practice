6af10c1e675c5f6d3d27c1e5b5ead7c3ca8c0b2d
Samandar000 <samandarnfh.2020@gmail.com>
2023-01-06 23:32:25 +0500

    finance

--- finance.py
+++ finance.py
@@ -11,7 +11,12 @@ class Finance(Randommer):
         Returns:
             list: list of types
         '''
-        pass
+        self.base_url = f'{self.get_url()}Finance/CryptoAddress/Types'
+        header = {
+            'X-Api-Key':api_key
+        }
+        response = requests.get(self.get_url(),headers=header)
+        return response.json()
 
     def get_crypto_address(self, crypto_type: str, api_key: str) -> dict:
         '''get available crypto address
@@ -23,7 +28,16 @@ class Finance(Randommer):
         Returns:
             dict: crypto address
         '''
-        pass
+        self.base_url = f'{self.get_url()}Finance/CryptoAddress'
+        payload = {
+            'cryptoType':crypto_type
+        }
+        header = {
+            'X-Api-Key':api_key
+        }
+
+        response = requests.get(self.get_url(),params=payload,headers=header)
+        return response.json()
 
     def get_countries(self, api_key: str) -> list:
         '''get available countries
@@ -34,7 +48,12 @@ class Finance(Randommer):
         Returns:
             list: crypto address
         '''
-        pass
+        self.base_url = f'{self.get_url()}Finance/Countries'
+        header = {
+            'X-Api-Key':api_key
+        }
+        response = requests.get(self.get_url(), headers=header)
+        return response.json()
 
     def get_iban_by_country_code(self, country_code: str, api_key: str) -> dict:
         '''get available countries
@@ -46,4 +65,12 @@ class Finance(Randommer):
         Returns:
             dict: idan data
         '''
-        pass
+        self.base_url = f'{self.get_url()}Finance/Iban/{country_code}'
+        payload = {
+            'countryCode':country_code
+        }
+        header = {
+            'X-Api-Key':api_key
+        }
+        response = requests.get(self.get_url(),params=payload,headers=header)
+        return response
