08fba9a3e8ee507e6496bd20c8e1bbc56fd7d000
Samandar000 <samandarnfh.2020@gmail.com>
2023-01-07 23:23:57 +0500

    all

---
+++ test.py
@@ -0,0 +1,9 @@
+from card import Card
+from finance import Finance
+from misc import Misc
+from name import Name
+from phone import Phone
+from social_number import SocialNumber
+c = SocialNumber()
+print(c.get_SocialNumber('7ecb15569ae344958a04ba1c583bd86e'))
+
